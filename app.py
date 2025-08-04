e3f3421e5cb6a0acfac7baf76ff870c8ee9df877
Yousef Noor <yous3fnoor@gmail.com>
2025-08-04 18:34:05 -0500

    Fix crash by removing debug mode

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def update_task(task_id):
     return jsonify({"updated": result.modified_count > 0})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
